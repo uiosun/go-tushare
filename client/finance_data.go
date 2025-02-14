2c6629d617366ec8c844822df6ecacf8183ec7eb
大狼狗 <uiosun@outlook.com>
2025-02-14 11:07:39 +0800

    新增 Income 相关接口

--- client/finance_data.go
+++ client/finance_data.go
@@ -1,18 +1,22 @@
 package client
 
 // Income 获取上市公司财务利润表数据
-func (api *TuShare) Income(params map[string]string, fields []string) (*APIResponse, error) {
-	// Check params
-	_, hasTsCode := params["ts_code"]
-	if !hasTsCode {
-		return nil, ERR_ARGUEMENT
+func (api *TuShare) Income(params map[string]string, isVip bool) (*APIResponse, error) {
+	apiName := "income"
+	if !isVip {
+		// Check params
+		_, hasTsCode := params["ts_code"]
+		if !hasTsCode {
+			return nil, ERR_ARGUEMENT
+		}
+	} else {
+		apiName = "income_vip"
 	}
 
 	body := map[string]interface{}{
-		"api_name": "income",
+		"api_name": apiName,
 		"token":    api.token,
 		"params":   params,
-		"fields":   fields,
 	}
 
 	return api.postData(body)
